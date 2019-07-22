5a32bc43e54085bb49683029fb198d4a73fb8705
Peter Zhao <pjz1@williams.edu>
2019-07-22 19:03:00 -0400

    comments

--- lib/Edit/MetricLcs.ts
+++ lib/Edit/MetricLcs.ts
@@ -1,9 +1,9 @@
 import { string } from "space-lift/commonjs/object/is";
 
 /*
- * Return the length of Longest Common Subsequence (LCS) between strings s1
- * and s2.
- * Inspired by https://github.com/tdebatty/java-string-similarity/blob/1afdc76eef523b144c73be3c3fc7df88f93fe9b7/src/main/java/info/debatty/java/stringsimilarity/LongestCommonSubsequence.java
+ * Return the length of Longest Common Subsequence (LCS) between strings s1 and s2.
+ * Also returns the sequence of edits, starting with insertion, then deletions
+ * sorted by position
  * 
  * @param s1 the first string
  * @param s2 the second string
@@ -67,11 +67,3 @@ export function metriclcs(s1: string, s2: string): string[] {
   let c = "kittena";
   let d = "sittinb";
   console.log(metriclcs(c,d))
-  /*
-  let a = "this is a short sentence and this is a long sentence and this is a medium sentence Naive implementation based on wikipedia article on Wagner-Fischer Algorithm and text in Skiena pdfpdppdfad df Returns a new CharStream representing the head of the input at the current position.  Throws an exception if the CharStream isempty. asdfasdfoefjawepqoepkmvjcxnaikmlsdjfkmewa fkdfmadsf oewa f w49r23rmr p sat takes a predicate and yields a parser that consumes a* single character if the character satisfies the predicate,* otherwise it fails.fgsdgsfd sdfgsfgsfdgsdgdf=a-0g-e-34t=wr-gwreg ggg3gseg4g30w4g-irojg4gokwij43g gj 3-ko3rfw3r gjw3ij pjw3gwjg w43gjw4 g-93wjw-34g9-w43 jg4-w g-wjg-wjg4-w4gjw-3 g-94wjg-wj4-w4 gjg-jg-wjgw3g-jg rg34u 34h89t9gh0rghgrkgfgm m mdkandf pjff"
-  let b = "recursively finds the optimum string associated with a dynamic programming table Performs one instance of a levenshtein calculation given two strings, and returns the dynamic programming table Returns true if the input at the current position is empty. Notethat a CharStream at the end of the input contains an emptystring but that an empty string may not be the end-of-file (i.e.,isEOF is false). choice specifies an ordered choice between two parsers,* p1 and p2. The returned parser will first apply* parser p1.  If p1 succeeds, p1's Outcome is returned.* If p1 fails, p2 is applied and the Outcome of p2 is returned.** An exception is when an outcome is a critical failure,* that outcome is immediately returned.4 t0q34t9q-u4 -q=9gq3u43wu4-34g=3g39= gq3 g3g4g q4"
-  console.log("string length: " + a.length);
-  console.time("metric")
-  console.log("metric: " + metriclcs(b, a));
-  console.timeEnd("metric")
-  */
